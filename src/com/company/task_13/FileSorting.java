8f75680f68fbc206db4d692d22d1efb153433f2e
Gutu <egutu2@techteam.com>
2019-04-16 00:33:29 +0300

    changed task_13

--- src/com/company/task_13/FileSorting.java
+++ src/com/company/task_13/FileSorting.java
@@ -1,16 +1,30 @@
 package com.company.task_13;
 
 import java.io.File;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 
 public class FileSorting {
     public static void sorting(File file) {
-        File[] files = file.listFiles();
-        Arrays.sort(files, Comparator.comparing(File::isFile));
-        System.out.println(Arrays.toString(files));
+        List<File> list = new ArrayList<>();
+        for (File f : Objects.requireNonNull(file.listFiles()))
+            if (f.listFiles() != null) {
+                list.add(f);
+                Collections.addAll(list, Objects.requireNonNull(f.listFiles()));
+            }
+        File[] fileArray = new File[list.size()];
+        fileArray = list.toArray(fileArray);
+        Arrays.sort(fileArray, (File a, File b) ->
+                {
+                    int comparator = Boolean.compare(b.isDirectory(), a.isDirectory());
+                    if (comparator == 0)
+                        comparator = (a).compareTo(b);
+                    return comparator;
+                }
+        );
+        for (File f : fileArray)
+            System.out.println(f);
+
     }
 }
-// :: transmite functiile ca argument
 
 
