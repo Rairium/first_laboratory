a8ccb65d5ffe470e356d4fb4105ed7f269478b0c
Gutu <egutu2@techteam.com>
2019-04-16 00:40:27 +0300

    made exeption for negative numbers task_4

--- src/com/company/task_4/FactorialComputing.java
+++ src/com/company/task_4/FactorialComputing.java
@@ -4,6 +4,10 @@ import java.math.BigInteger;
 
 public class FactorialComputing {
     public static void showFactorial(int number) {
+        if (number < 0){
+            System.out.println("The factorial from a negative number cannot be calculated");
+            return;
+        }
         BigInteger bigResult = new BigInteger("1");
         for (int i = 1; i < number; i++){
             bigResult = bigResult.multiply(BigInteger.valueOf(i));
